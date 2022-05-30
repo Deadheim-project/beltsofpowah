10c26528756c456221d90d6e37f49b1cdca47bec
werner <werneralexandre@outlook.com>
2022-05-30 10:41:42 -0300

    shit

--- Plugin.cs
+++ Plugin.cs
@@ -8,6 +8,7 @@ using System.Reflection;
 using System.Linq;
 using System.IO;
 using System;
+using System.Collections.Generic;
 
 namespace BeltsOfPowah
 {
@@ -17,14 +18,15 @@ namespace BeltsOfPowah
     {
         ConfigSync configSync = new ConfigSync(PluginGUID) { DisplayName = PluginGUID, CurrentVersion = Version, MinimumRequiredVersion = Version };
 
-        public const string Version = "1.0.0";
-        public const string PluginGUID = "Detalhes.BeltsOfPowah";
-      
-        public static ConfigEntry<string> BlockedRockAndTreeDropAtEndAge;
-
-        public static GameObject dontCraftPrefab;
+        public const string Version = "1.0.3";
+        public const string PluginGUID = "Detalhes.BeltsOfPowah";      
 
         Harmony _harmony = new Harmony(PluginGUID);
+
+        public static Dictionary<string, ConfigEntry<float>> skillValue = new();
+        public static Dictionary<string, ConfigEntry<string>> beltText = new();
+
+        static List<GameObject> belts = new();
 
         ConfigEntry<T> config<T>(string group, string name, T value, ConfigDescription description, bool synchronizedSetting = true)
         {
@@ -35,6 +37,7 @@ namespace BeltsOfPowah
 
             return configEntry;
         }
+
         ConfigEntry<T> config<T>(string group, string name, T value, string description, bool synchronizedSetting = true) => config(group, name, value, new ConfigDescription(description), synchronizedSetting);
 
         [HarmonyPatch(typeof(ConfigSync), "RPC_ConfigSync")]
@@ -44,6 +47,32 @@ namespace BeltsOfPowah
             private static void Postfix()
             {
                 if (ZNet.instance.IsServer()) return;
+
+                foreach (var item in belts)
+                {
+                    ItemDrop itemDrop = item.GetComponent<ItemDrop>();
+                    SE_Stats effect = (SE_Stats)itemDrop.m_itemData.m_shared.m_equipStatusEffect;
+
+                    effect.m_skillLevelModifier = skillValue[effect.name.ToLower()].Value;
+                    itemDrop.m_itemData.m_shared.m_description = beltText[effect.name.ToLower()].Value;
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(ZNetScene), "Awake")]
+        public static class OnSpawned
+        {
+            [HarmonyPriority(Priority.Last)]
+            private static void Postfix()
+            {
+                foreach (var item in belts)
+                {
+                    ItemDrop itemDrop = item.GetComponent<ItemDrop>();
+                    SE_Stats effect = (SE_Stats)itemDrop.m_itemData.m_shared.m_equipStatusEffect;
+
+                    effect.m_skillLevelModifier = skillValue[effect.name.ToLower()].Value;
+                    itemDrop.m_itemData.m_shared.m_description = beltText[effect.name.ToLower()].Value;
+                }
             }
         }
 
@@ -66,6 +95,8 @@ namespace BeltsOfPowah
             CreatePrefab("BeltOfClubs");
             CreatePrefab("BeltOfPolearm");
             CreatePrefab("BeltOfSpears");
+            CreatePrefab("beltOfGreatMagicResistence");
+            CreatePrefab("beltOfMagicResistence");
         }
 
         unsafe private void CreatePrefab(string name)
@@ -76,6 +107,10 @@ namespace BeltsOfPowah
             item.RequiredItems.Add("LeatherScraps", 50);
             item.RequiredItems.Add("WolfPelt", 10);
             item.Crafting.Add(CraftingTable.Workbench, 2);
-        } 
+            SE_Stats effect = (SE_Stats) item.Prefab.GetComponent<ItemDrop>().m_itemData.m_shared.m_equipStatusEffect;
+            belts.Add(item.Prefab);
+            skillValue.Add(effect.name.ToLower(), config("1 - Skill", $"{name}", effect.m_skillLevelModifier, name));
+            beltText.Add(effect.name.ToLower(),config("1 - Text", $"{name}", item.Prefab.GetComponent<ItemDrop>().m_itemData.m_shared.m_description.Replace('\'', ' '), name));
+        }
     }
 }
